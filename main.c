7fc60da25ddd3a82c96550b5422babb9e98f03f6
Helytre <matthieu.tironneau@efrei.net>
2025-03-26 17:58:07 +0100

    free in main pls

--- main.c
+++ main.c
@@ -3,10 +3,9 @@
 
 int main() {
     char* filename = "quotes.txt";
-    char* fil[] = {"aret", "ar"};
-    print_random_quote(fil);
     char** p_p_quotes = load_quotes(filename);
 
+    free_quotes(p_p_quotes);
     srand(time(NULL));
     return 0;
 }
