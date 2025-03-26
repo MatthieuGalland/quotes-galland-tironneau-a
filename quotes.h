85321a9c088a16cdf11833b9b66bc7238e5bcbbb
Helytre <matthieu.tironneau@efrei.net>
2025-03-26 17:35:47 +0100

    creation of load quote

--- quotes.h
+++ quotes.h
@@ -7,8 +7,10 @@
 #include <string.h>
 
 #define MAX_QUOTES 10
+#define MAX_QUOTE_SIZE 100
 
 void print_quote(char ** quotes, int index);
 void print_random_quote(char ** quotes);
+char** load_quotes(char* filename);
 
 #endif
